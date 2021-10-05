1bed0cd3560d26b6d89710f16bfe27c7b069db53
tanthammar <tinahammar@gmail.com>
2021-10-05 15:43:05 +0200

    Requires upcoming Tall-forms v8

--- stubs/class.stub.php
+++ stubs/class.stub.php
@@ -10,40 +10,44 @@ class DummyModelForm extends TallFormComponent
 {
     public function mount(?DummyModel $dummymodel)
     {
-        //Gate::authorize()
-        $this->fill([
+        //Gate::authorize();
+        $this->mount_form($dummymodel); // $dummymodel from hereon, called $this->model
+    }
+
+    protected function formAttr(): array
+    {
+        return [
             'formTitle' => 'Create & Edit DummyModel',
             'wrapWithView' => true, //see https://github.com/tanthammar/tall-forms/wiki/installation/Wrapper-Layout
             'showSave' => true,
             'showReset' => true,
             'showDelete' => $dummymodel->exists ? true : false, //see https://github.com/tanthammar/tall-forms/wiki/Form-Methods#delete
             'showGoBack' => true,
-        ]);
-        $this->mount_form($dummymodel); // $dummymodel from hereon, called $this->model
+        ];
     }
 
 
     // REQUIRED, if you are creating a model with this form
-    public function onCreateModel($validated_data)
+    protected function onCreateModel($validated_data)
     {
         // create...
     }
 
     // OPTIONAL, method exists in tall-form component
-    public function onUpdateModel($validated_data)
+    protected function onUpdateModel($validated_data)
     {
         // update...
     }
 
     // OPTIONAL, method exists in tall-form component
-    public function onDeleteModel()
+    protected function onDeleteModel()
     {
         $dummymodel = $this->model;
         // delete...
     }
 
 
-    public function fields()
+    protected function fields()
     {
         return [
             // fields...
